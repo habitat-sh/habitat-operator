b6951c57342555b9324a3e8f1bf19ed63a8d8e77
Lili Cosic <lili@kinvolk.io>
2017-09-04 13:51:24 +0200

    test: switch to apiv1
    Due to the switching to client-go v4.0.0, the api changes made the tests
    fail. This adjustment fixes that.

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -21,9 +21,9 @@ import (
 
 	crv1 "github.com/kinvolk/habitat-operator/pkg/habitat/apis/cr/v1"
 
-	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
