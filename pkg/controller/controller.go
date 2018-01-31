4e36d9776f48c40622a7bea6935eff08230db2aa
Lorenzo Manacorda <lorenzo@kinvolk.io>
2018-01-31 16:35:11 +0100

    Remove subdir from mount path
    It was creating a directory named user.toml and placing the user.toml
    there.
    
    Signed-off-by: Lorenzo Manacorda <lorenzo@kinvolk.io>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -714,7 +714,7 @@ func (hc *HabitatController) newDeployment(h *habv1beta1.Habitat) (*appsv1beta1.
 			Name: initialConfigFilename,
 			// The Habitat supervisor creates a directory for each service under /hab/svc/<servicename>.
 			// We need to place the user.toml file in there in order for it to be detected.
-			MountPath: fmt.Sprintf("/hab/user/%s/config/%s", h.Spec.Service.Name, userTOMLFile),
+			MountPath: fmt.Sprintf("/hab/user/%s/config", h.Spec.Service.Name),
 			ReadOnly:  false,
 		}
 
