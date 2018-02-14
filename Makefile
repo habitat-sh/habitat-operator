habitat-sh/habitat-operator

commit 4d7775336c4f9da28c752809797f6a42d7bd0942

Add helm chart for the operator
This is the source for the Helm chart. You can use:

helm package helm/habitat-operator

command to create a chart package for the operator for distribution.

This chart uses the docker image of the latest release: 0.4.0.

Makefile
@@ -28,7 +28,13 @@ clean-test:
 	kubectl delete clusterrole habitat-operator
 
 update-version:
-	find examples -name "*.yml" -type f -exec sed -i.bak "s/habitat-operator:.*/habitat-operator:v$$(cat VERSION)/g" '{}' \;
-	find examples -name "*.yml.bak" -type f -exec rm '{}' \;
+	find examples helm -name "*.yml" -o -name "*.yaml" -type f \
+		-exec sed -i.bak \
+		-e "s/habitat-operator:.*/habitat-operator:v$$(cat VERSION)/g" \
+		-e "s/tag:.*/tag: v$$(cat VERSION)/g" \
+		-e "s/version:.*/version: $$(cat VERSION)/g" \
+		'{}' \;
+	find examples helm -name "*.yml.bak" -o -name "*.yaml.bak" -type f \
+		-exec rm '{}' \;
 
 .PHONY: build test linux image e2e clean-test update-version

helm/habitat-operator/.helmignore
@@ -1,0 +1,21 @@
+# Patterns to ignore when building packages.
+# This supports shell glob matching, relative path matching, and
+# negation (prefixed with !). Only one pattern per line.
+.DS_Store
+# Common VCS dirs
+.git/
+.gitignore
+.bzr/
+.bzrignore
+.hg/
+.hgignore
+.svn/
+# Common backup files
+*.swp
+*.bak
+*.tmp
+*~
+# Various IDEs
+.project
+.idea/
+*.tmproj

helm/habitat-operator/Chart.yaml
@@ -1,0 +1,10 @@
+apiVersion: v1
+description: A Kubernetes controller that manages Habitat services in a Kubernetes cluster.
+engine: gotpl
+maintainers:
+  - name: Zeeshan Ali
+    email: zeeshan@kinvolk.io
+name: habitat-operator
+sources:
+  - https://github.com/kinvolk/habitat-operator
+version: 0.4.0

helm/habitat-operator/templates/_helpers.tpl
@@ -1,0 +1,21 @@
+{{/* vim: set filetype=mustache: */}}
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "habitat-operator.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "habitat-operator.fullname" -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}

helm/habitat-operator/templates/clusterrole.yaml
@@ -1,0 +1,49 @@
+{{- if .Values.rbacEnable }}
+{{- if .Capabilities.APIVersions.Has "rbac.authorization.k8s.io/v1beta1" }}
+apiVersion: rbac.authorization.k8s.io/v1beta1
+{{- else if .Capabilities.APIVersions.Has "rbac.authorization.k8s.io/v1alpha1" }}
+apiVersion: rbac.authorization.k8s.io/v1alpha1
+{{- else if .Capabilities.APIVersions.Has "rbac.authorization.k8s.io/v1" }}
+apiVersion: rbac.authorization.k8s.io/v1
+{{- end }}
+kind: ClusterRole
+metadata:
+  labels:
+    app: {{ template "habitat-operator.name" . }}
+    chart: {{ .Chart.Name }}-{{ .Chart.Version }}
+    heritage: {{ .Release.Service }}
+    release: {{ .Release.Name }}
+  name: {{ template "habitat-operator.fullname" . }}
+rules:
+- apiGroups:
+  - apiextensions.k8s.io
+  resources:
+  - customresourcedefinitions
+  verbs: ["get", "list", "watch", "create", "update", "patch", "delete"]
+- apiGroups:
+  - habitat.sh
+  resources:
+  - habitats
+  verbs: ["get", "list", "watch", "create", "update", "patch", "delete"]
+- apiGroups:
+  - apps
+  resources:
+  - deployments
+  verbs: ["get", "list", "watch", "create", "update", "patch", "delete"]
+- apiGroups: [""]
+  resources:
+  - configmaps
+  verbs: ["get", "list", "watch", "create", "update", "patch", "delete"]
+- apiGroups: [""]
+  resources:
+  - secrets
+  verbs: ["get"]
+- apiGroups: [""]
+  resources:
+  - pods
+  verbs: ["get", "list", "watch"]
+- apiGroups: [""]
+  resources:
+  - namespaces
+  verbs: ["list"]
+{{- end }}

helm/habitat-operator/templates/clusterrolebinding.yaml
@@ -1,0 +1,25 @@
+{{- if .Values.rbacEnable }}
+{{- if .Capabilities.APIVersions.Has "rbac.authorization.k8s.io/v1beta1" }}
+apiVersion: rbac.authorization.k8s.io/v1beta1
+{{- else if .Capabilities.APIVersions.Has "rbac.authorization.k8s.io/v1alpha1" }}
+apiVersion: rbac.authorization.k8s.io/v1alpha1
+{{- else if .Capabilities.APIVersions.Has "rbac.authorization.k8s.io/v1" }}
+apiVersion: rbac.authorization.k8s.io/v1
+{{- end }}
+kind: ClusterRoleBinding
+metadata:
+  labels:
+    app: {{ template "habitat-operator.name" . }}
+    chart: {{ .Chart.Name }}-{{ .Chart.Version }}
+    heritage: {{ .Release.Service }}
+    release: {{ .Release.Name }}
+  name: {{ template "habitat-operator.fullname" . }}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: {{ template "habitat-operator.fullname" . }}
+subjects:
+  - kind: ServiceAccount
+    name: {{ template "habitat-operator.fullname" . }}
+    namespace: {{ .Release.Namespace }}
+{{- end }}

helm/habitat-operator/templates/deployment.yaml
@@ -1,0 +1,32 @@
+apiVersion: apps/v1beta1
+kind: Deployment
+metadata:
+  labels:
+    app: {{ template "habitat-operator.name" . }}
+    chart: {{ .Chart.Name }}-{{ .Chart.Version }}
+    heritage: {{ .Release.Service }}
+    operator: habitat
+    release: {{ .Release.Name }}
+  name: {{ template "habitat-operator.fullname" . }}
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: {{ template "habitat-operator.name" . }}
+        operator: habitat
+        release: {{ .Release.Name }}
+    spec:
+      containers:
+        - name: {{ template "habitat-operator.name" . }}
+          image: "{{ .Values.image.repository }}:{{ .Values.image.tag }}"
+          imagePullPolicy: "{{ .Values.image.pullPolicy }}"
+          resources:
+{{ toYaml .Values.resources | indent 12 }}
+    {{- if .Values.nodeSelector }}
+      nodeSelector:
+{{ toYaml .Values.nodeSelector | indent 8 }}
+    {{- end }}
+    {{- if .Values.rbacEnable }}
+      serviceAccountName: {{ template "habitat-operator.fullname" . }}
+    {{- end }}

helm/habitat-operator/templates/serviceaccount.yaml
@@ -1,0 +1,11 @@
+{{- if .Values.rbacEnable }}
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  labels:
+    app: {{ template "habitat-operator.name" . }}
+    chart: {{ .Chart.Name }}-{{ .Chart.Version }}
+    heritage: {{ .Release.Service }}
+    release: {{ .Release.Name }}
+  name: {{ template "habitat-operator.fullname" . }}
+{{- end }}

commit 65dd70981c51c4c8b57f2784c42f0c867bc246a0

Separate version update cmd for Helm files
The rule to update the version string all the YAML files was broken from
4d7775336c4f9da28c752809797f6a42d7bd0942, where we tried to use the '-o'
option of `find` command replace version string in both '*.yaml' and
'*.yml' files. Turns out, that doesn't work for some reason.

So let's use a separate `find` commandline for Helm and example files.

Makefile
@@ -28,13 +28,16 @@ clean-test:
 	kubectl delete clusterrole habitat-operator
 
 update-version:
-	find examples helm -name "*.yml" -o -name "*.yaml" -type f \
+	find examples -name "*.yml" -type f \
 		-exec sed -i.bak \
 		-e "s/habitat-operator:.*/habitat-operator:v$$(cat VERSION)/g" \
+		'{}' \;
+	find helm -name "*.yaml" -type f \
+		-exec sed -i.bak \
 		-e "s/tag:.*/tag: v$$(cat VERSION)/g" \
 		-e "s/version:.*/version: $$(cat VERSION)/g" \
 		'{}' \;
-	find examples helm -name "*.yml.bak" -o -name "*.yaml.bak" -type f \
+	find examples helm -regex '.*\.ya?ml$$.bak' -type f \
 		-exec rm '{}' \;
 
 .PHONY: build test linux image e2e clean-test update-version

examples/habitat-operator-deployment.yml
@@ -11,4 +11,4 @@ spec:
     spec:
       containers:
       - name: habitat-operator
-        image: kinvolk/habitat-operator:v0.4.0
+        image: kinvolk/habitat-operator:v0.5.0

examples/rbac/habitat-operator.yml
@@ -11,5 +11,5 @@ spec:
     spec:
       containers:
       - name: habitat-operator
-        image: kinvolk/habitat-operator:v0.4.0
+        image: kinvolk/habitat-operator:v0.5.0
       serviceAccountName: habitat-operator