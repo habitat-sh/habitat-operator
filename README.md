810bcc1e06333842b096a95755bc78a33d958d76
Krzesimir Nowak <krzesimir@kinvolk.io>
2018-02-15 13:47:21 +0100

    Update the script and instructions

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # The habitat-operator helm charts
 
-This directory hosts all the helm charts related to the habitat-operator.
+The `helm/charts` directory hosts all the helm charts related to the habitat-operator.
 
-To add the chart for a new release of the habitat-operator run the following from the `helm/charts` directory:
+To add the chart for a new release of the habitat-operator run the `chart-for-latest.sh` script - it's in the toplevel directory. The script will create a new commit for you to review. If everything looks fine, feel free to push the changes:
 
 ```console
-$ git merge master
-$ ./chart-for-latest.sh
 $ git push origin gh-pages
 ```
 
--- chart-for-latest.sh
+++ chart-for-latest.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-helm package ../habitat-operator
-mv habitat-operator-$(cat ../../VERSION).tgz stable/
-helm repo index stable --url https://kinvolk.github.io/habitat-operator/helm/charts/stable/ --merge stable/index.yaml
-git add stable/index.yaml stable/habitat-operator-$(cat ../../VERSION).tgz
-git commit -m "Add helm chart for $(cat ../../VERSION)"
+dir=$(dirname $0)
+
+git -C "${dir}" checkout master -- VERSION helm/habitat-operator
+# Drop unwanted/untracked contents in the helm/habitat-operator
+# directory, so they won't end up in the chart file. This will not
+# remove new files that appeared after the "git checkout" command,
+# because they are also immediately staged to be commited.
+git -C "${dir}" clean -ffdx -- helm/habitat-operator
+
+stablesubdir='helm/charts/stable'
+version=$(cat "${dir}/VERSION")
+
+helm package "${dir}/helm/habitat-operator"
+mv "habitat-operator-${version}.tgz" "${dir}/${stablesubdir}/"
+helm repo index stable --url "https://kinvolk.github.io/habitat-operator/${stablesubdir}/" --merge "${dir}/${stablesubdir}/index.yaml"
+git -C "${dir}" add "${stablesubdir}/index.yaml" "${stablesubdir}/habitat-operator-${version}.tgz"
+git -C "${dir}" commit -m "Add helm chart for ${version}"
