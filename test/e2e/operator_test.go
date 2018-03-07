17597cf457df55973166d9ebada8d66c88e89e31
Lorenzo Manacorda <lorenzo@kinvolk.io>
2018-03-07 16:43:51 +0100

    Skip persistence tests
    They cannot be run in our current test setup, i.e. minikube VMs on
    Travis.
    
    Signed-off-by: Lorenzo Manacorda <lorenzo@kinvolk.io>

--- test/e2e/operator_test.go
+++ test/e2e/operator_test.go
@@ -187,6 +187,9 @@ func TestHabitatDelete(t *testing.T) {
 }
 
 func TestHabitatPersistence(t *testing.T) {
+	// We run minikube in a VM on Travis. In that environment, we cannot create PersistentVolumes.
+	t.Skip("This test cannot be run successfully in our current testing setup")
+
 	ephemeral, err := utils.ConvertHabitat("resources/standalone/habitat.yml")
 	if err != nil {
 		t.Fatal(err)
