43ec99f219d024cd72128396e6d218f133936d32
Lorenzo Manacorda <lorenzo@kinvolk.io>
2018-03-13 15:28:51 +0100

    Add hack/verify-codegen.sh script
    Can be run in a CI.
    
    Signed-off-by: Lorenzo Manacorda <lorenzo@kinvolk.io>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copyright 2017 The Kubernetes Authors.
+# Copyright 2018 Chef Software Inc. and/or applicable contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Based on https://github.com/kubernetes/code-generator/blob/893b4433a4ba929dd0dacebf7c8956682d7a5d5f/hack/update-codegen.sh
 
 # First of all, the code-generator needs to be fetched:
 # $ go get -u k8s.io/code-generator
