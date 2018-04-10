147d48948922ca96ebe8de1f89c8dc87e375f50a
Lorenzo Manacorda <lorenzo@kinvolk.io>
2018-04-10 14:23:43 +0200

    habitat: Import v1beta2 type additions to v1beta1
    This commit imports the additions to the v1beta1 type, which is a step
    in the direction of supporting multiple "virtual" types with just one
    concrete type.

--- pkg/apis/habitat/v1beta1/types.go
+++ pkg/apis/habitat/v1beta1/types.go
@@ -53,6 +53,20 @@ type HabitatSpec struct {
 	// The EnvVar type is documented at https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.9/#envvar-v1-core.
 	// Optional.
 	Env []corev1.EnvVar `json:"env,omitempty"`
+	// Optional.
+	PersistentStorage *PersistentStorage `json:"persistentStorage,omitempty"`
+}
+
+// PersistentStorage contains the details of the persistent storage that the
+// cluster should provision.
+type PersistentStorage struct {
+	// Size is the volume's size.
+	// It uses the same format as Kubernetes' size fields, e.g. 10Gi
+	Size string `json:"size"`
+	// MountPath is the path at which the PersistentVolume will be mounted.
+	MountPath string `json:"mountPath"`
+	// StorageClassName is the name of the StorageClass that the StatefulSet will request.
+	StorageClassName string `json:"storageClassName"`
 }
 
 type HabitatStatus struct {
